4a2d202ecfd8270dc8b53327696bafacf9bdccba
Vladimir <vladimir.legacy@gmail.com>
2018-12-08 11:51:06 +0300

    [NEW] initial commit

---
+++ ImageFilter/PresentationLayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageFilter
+//
+//  Created by Vladimir Gordienko on 08/12/2018.
+//  Copyright © 2018 Vladimir Gordienko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
